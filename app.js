002452c04df1a570b5f5e3ec532ed37d0e5636cb
DESKTOP-3P4ESAE\Kuba <kuba9519@gmail.com>
2021-02-03 11:21:51 +0100

    Finish base assignment.

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-import { route } from './router';
+import { route } from "./router";
 
-route('/', 'home', function() {
-  this.where = 'here';
+const signIn = async (username, password) => {
+  const response = await fetch("https://zwzt-zadanie.netlify.app/api/login", {
+    method: "POST",
+    body: JSON.stringify({ username, password }),
+  });
+
+  return await response.json();
+}
+
+route("/", "home", function () {
+  this.error = "";
+  this.$on("#form", "submit", async (e) => {
+    e.preventDefault();
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    const data = await signIn(username, password);
+
+    if (data.error) {
+      this.error = data.message;
+      this.$refresh();
+    } else {
+      window.location.href = "#/success";
+      localStorage.setItem("token", data.token);
+    }
+  });
 });
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
+route("/success", "success", function () {});
+
+route("/ex1", "example1", function () {
+  this.title = "Example 1";
 });
 
-route('/ex2', 'example2', function() {
-  this.title = 'Example 2';
+route("/ex2", "example2", function () {
+  this.title = "Example 2";
   this.counter = 0;
-  this.$on('.my-button', 'click', () => {
+  this.$on(".my-button", "click", () => {
     this.counter += 1;
     this.$refresh();
   });
 });
 
-route('*', '404', function () {});
+route("*", "404", function () {});
